2a694079b332bc058306535fe0d7dff659ca4cea
Anastasiia Dosyn <anastasiia.dosyn@student.tuke.sk>
2024-06-16 18:29:25 +0200

    added and edited doGet method in LogicServlet class

---
+++ src/main/java/com/tictactoe/LogicServlet.java
@@ -0,0 +1,61 @@
+package com.tictactoe;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "LogicServlet", value = "/logic")
+public class LogicServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession currentSession = req.getSession();
+        Field currentField = extractField(currentSession);
+
+        int index = getSelectedIndex(req);
+        Sign currentSign = currentField.getField().get(index);
+
+        if (Sign.EMPTY != currentSign) {
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/index.jsp");
+            dispatcher.forward(req, resp);
+            return;
+        }
+        currentField.getField().put(index, Sign.CROSS);
+
+        int emptyFieldIndex = currentField.getEmptyFieldIndex();
+
+        if (emptyFieldIndex >= 0) {
+            currentField.getField().put(emptyFieldIndex, Sign.NOUGHT);
+        }
+
+        List<Sign> data = currentField.getFieldData();
+
+        currentSession.setAttribute("data", data);
+        currentSession.setAttribute("field", currentField);
+
+        resp.sendRedirect("/index.jsp");
+    }
+
+    private int getSelectedIndex(HttpServletRequest request) {
+        String click = request.getParameter("click");
+        if (click != null && click.matches("\\d+")) {
+            return Integer.parseInt(click);
+        } else {
+            return 0;
+        }
+    }
+
+    private Field extractField(HttpSession session) {
+        Object field = session.getAttribute("field");
+        if(Field.class != field.getClass()) {
+            session.invalidate();
+            throw new RuntimeException("Session is broken, try one more time");
+        }
+        return (Field) field;
+    }
+}

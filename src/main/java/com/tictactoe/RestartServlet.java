1c6c05795bb44ccc4068813c4f8578cbc0f0ebb2
Anastasiia Dosyn <anastasiia.dosyn@student.tuke.sk>
2024-06-16 19:49:36 +0200

    RestartServlet added

--- src/main/java/com/tictactoe/RestartServlet.java
+++ src/main/java/com/tictactoe/RestartServlet.java
@@ -1,6 +1,5 @@
 package com.tictactoe;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
